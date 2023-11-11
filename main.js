0cb120d85290e271c058eb369725d7a8092437ae
ninjaluke45 <eskymonkey@gmail.com>
2023-11-11 16:13:26 -0700

    added functions to man.js

--- main.js
+++ main.js
@@ -23,3 +23,79 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
+// GetScores
+apiRouter.get('/scores', (_req, res) => {
+  res.send(scores);
+});
+
+// SubmitScore
+apiRouter.post('/score', (req, res) => {
+  scores = updateScores(req.body, scores);
+  res.send(scores);
+});
+
+// GetPlayers
+apiRouter.get('/players', (_req, res) => {
+  res.send(players);
+});
+
+// SubmitPlayers
+apiRouter.post('/player', (req, res) => {
+  players = updatePlayers(req.body, scores);
+  res.send(players);
+});
+
+let scores = [];
+
+function compScore(score){
+  tot = score.total;
+  wins = score.win;
+
+  perc = (wins/tot) * 100;
+  return Math.round(perc);
+
+}
+
+function updateScores(newScore, scores) {
+  // this is where a third party service call could be made
+  // third party service would take priorty but if failed would use below code
+
+  let found = false;
+  for (const [i, prevScore] of scores.entries()) {
+    if (compScore(newScore) > compScore(prevScore)) {
+      scores.splice(i, 0, newScore);
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    scores.push(newScore);
+  }
+
+ 
+
+  return scores;
+}
+
+
+let players = [];
+function updatePlayers(player) {
+// a third party service call could be made here
+
+  let found = false;
+  for (const [i, oldPlayer] of players.entries()) {
+    if (player.name === oldPlayer.name) {
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    players.push(player);
+  }
+
+
+  return players;
+}
+
